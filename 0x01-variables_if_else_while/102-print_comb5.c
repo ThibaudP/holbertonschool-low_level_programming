2a6ea43f190e2ca02fab2e10e143aabc7f1f4e43
ThibaudP <t.poncin@gmail.com>
2020-09-17 10:58:02 +0000

    Added 102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - Toto
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int i = 0;
+	int j, k;
+	int l, m, n;
+
+	while (i < 100)
+	{
+		j = i / 10;
+		k = i % 10;
+		l = 0;
+
+		while (l < 100)
+		{
+			m = l / 10;
+			n = l % 10;
+			putchar(j + '0');
+			putchar(k + '0');
+			putchar(' ');
+			putchar(m + '0');
+			putchar(n + '0');
+			if (i < 99 || l < 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			l++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
