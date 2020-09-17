f981f43fbbd2d85c777aa0202dbbd883e6601758
ThibaudP <t.poncin@gmail.com>
2020-09-17 10:33:01 +0000

    Added 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Toto
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+
+	while (i < 9)
+	{
+		j = i + 1;
+		while (j < 10)
+		{
+			k = j + 1;
+			while (k < 10)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+
+				if (i < 7 || j < 8 || k < 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				k++;
+			}
+		j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
