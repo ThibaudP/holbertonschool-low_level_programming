e94d1853c08d94063fba7b82eedbe6a1623900d9
ThibaudP <t.poncin@gmail.com>
2020-09-21 09:41:56 +0000

    first commit

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * _putchar - Prints "Holberton"
+ *
+ * Return: Always 0
+ */
+
+int _putchar(void)
+{
+	printf("Holberton\n");
+	return (0);
+}
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	_putchar();
+	return (0);
+}
