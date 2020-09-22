87685f08b4b9ca1342e4dc57c085febe8d8d72fb
ThibaudP <t.poncin@gmail.com>
2020-09-22 07:43:57 +0000

    Fixed 101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -14,11 +14,11 @@ int main(void)
 
 	for (i = 0; i < 1024; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if (i % 3 == 0 || i % 5 == 0)
 		{
 			tot = tot + i;
 		}
 	}
-	printf("%d\n", tot);
+	printf("%i\n", tot);
 	return (0);
 }
