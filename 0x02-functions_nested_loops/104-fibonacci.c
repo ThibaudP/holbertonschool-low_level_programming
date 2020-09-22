e4c9d91f3dac31edbc354a14235df33a0c9f9776
ThibaudP <t.poncin@gmail.com>
2020-09-22 16:15:53 +0000

    Added in depth description

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -3,19 +3,24 @@
 #include "holberton.h"
 
 /**
- * main - Entry point
+ * main - Compute the first 98 Fibonacci numbers without using long longs
+ *
+ * 1st for loop computes #2-#92 (93 overflows the unsigned int)
+ * Setup for 2nd loop splits the previous value & the previous previous
+ * at an arbitrary 1 billion (plenty enough to not overflow either half).
+ * Both halves are then added together separately. If the 2nd half overflows
+ * our 1B limit, we only keep the 9 rightmost digits and carry over +1
+ * to the 1st half. Then display the result.
  *
  * Return: Always 0
  */
 
 int main(void)
 {
-	unsigned long prepre, pre, cur, prepre_half1, prepre_half2;
+	unsigned long prepre = 1, pre = 2, cur, prepre_half1, prepre_half2;
 	unsigned long pre_half1, pre_half2, cur_half1, cur_half2;
 	int i = 2;
 
-	prepre = 1;
-	pre = 2;
 	printf("1, 2, ");
 
 	for (i = 2; i < 92; i++)
@@ -25,15 +30,16 @@ int main(void)
 		prepre = pre;
 		pre = cur;
 	}
+
 	pre_half1 = pre / 1000000000;
 	pre_half2 = pre % 1000000000;
 	prepre_half1 = prepre / 1000000000;
 	prepre_half2 = prepre % 1000000000;
+
 	for (i = 93; i < 99; i++)
 	{
 		cur_half1 = prepre_half1 + pre_half1;
 		cur_half2 = prepre_half2 + pre_half2;
-
 		if (prepre_half2 + pre_half2 > 999999999)
 		{
 			cur_half1 = cur_half1 + 1;
