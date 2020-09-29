555fabdc1c42bed6cce8c3765e100e28fff18775
ThibaudP <t.poncin@gmail.com>
2020-09-29 17:31:54 +0000

    Added 101

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - creates a keygen for 101-crackme
+ *
+ */
+
+int main(void)
+{
+	char arr[63] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	char pwd[100];
+	int i, val, sum = 0;
+
+	srand(time(0));
+
+	for (i = 0; sum < (2772 - 122); i++)
+	{
+		val = rand() % 63;
+		pwd[i] = arr[val];
+		sum += pwd[i];
+	}
+	pwd[i] = 2772 - sum;
+
+	printf("%s", pwd);
+	return (0);
+}
