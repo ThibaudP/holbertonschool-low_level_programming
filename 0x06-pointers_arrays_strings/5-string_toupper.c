0ebf200d270143bb502fbd9da375216eb8bc4d54
ThibaudP <t.poncin@gmail.com>
2020-10-01 13:32:00 +0000

    Added 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * _islower - Checks if a char is lowercase
+ * @c: a char
+ *
+ * Return: 1 if c is lowercase, 0 if not
+ */
+
+int _islower(int c)
+{
+	return ((c >= 'a' && c <= 'z') ? (1) : (0));
+}
+
+/**
+ * string_toupper - Changes all lowercase chars to uppercase
+ *
+ * @s: a string
+ *
+ * Return: the string in uppercase.
+ */
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i++])
+		if (_islower(s[i]))
+			s[i] -= 32;
+
+	return (s);
+}
