449e1b70513cbba64dfac02954b6147090a20d3a
ThibaudP <t.poncin@gmail.com>
2020-10-01 13:08:28 +0000

    Fixed 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -32,10 +32,10 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n && src[i]; i++)
 		dest[i] = src[i];
-	while (i < n)
-		dest[i + 1] = '\0';
+	for ( ; i < n; i++)
+		dest[i] = '\0';
 
 	return (dest);
 }
