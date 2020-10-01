dd0326caa06b6f5238929dd741c46ede1a4bf69d
ThibaudP <t.poncin@gmail.com>
2020-10-01 14:00:58 +0000

    4dd3d 74sk 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,18 +1,6 @@
 #include "holberton.h"
 
 /**
- * _islower - Checks if a char is lowercase
- * @c: a char
- *
- * Return: 1 if c is lowercase, 0 if not
- */
-
-int _islower(int c)
-{
-	return ((c >= 'a' && c <= 'z') ? (1) : (0));
-}
-
-/**
  * leet - translates string to leetspeak
  *
  * @s: a string
@@ -22,7 +10,7 @@ int _islower(int c)
 
 char *leet(char *s)
 {
-	int i = 0, j = 0;
+	int i = 0, j;
 	char *src = "aeotlAEOTL";
 	char *dest = "4307143071";
 
