35d82688f22039184635b863595f0d8ae4b497ed
ThibaudP <t.poncin@gmail.com>
2020-10-05 13:12:00 +0000

    Fix 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,10 +13,13 @@ char *_strchr(char *s, char c)
 {
 	unsigned int i = 0;
 
-	while (s[i++])
+	while (s[i])
 	{
 		if (s[i] == c)
 			return (s + i);
+		i++;
 	}
+	if (s[i] == c)
+		return (s + i);
 	return (0);
 }
