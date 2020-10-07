7f23e2ad0f01954ce79bfeb74d862fd0b939f9b0
ThibaudP <t.poncin@gmail.com>
2020-10-07 10:23:08 +0000

    Fixed 100

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -19,7 +19,9 @@ int wildcmp(char *s1, char *s2)
 	{
 		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
 			return (1);
-		if (s2 == '\0' && *(s1 + 1) != '\0')
+		else if (*s1 == '\0')
+			return (wildcmp(s1, s2 + 1));
+		else if (*(s1 + 1) != '\0')
 			return (0);
 	}
 
