fd067655994fb0881de88febf77511a4bbdb76f9
ThibaudP <t.poncin@gmail.com>
2020-10-13 14:46:43 +0000

    Added 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,55 +1,6 @@
 #include "holberton.h"
 
 /**
- * _strlen  - Returns the length of a string s
- *
- * @s: pointer to a string
- *
- * Return: the length of string s
- */
-
-int _strlen(char *s)
-{
-	int len = 0;
-
-	if (s == NULL)
-		return (0);
-
-	while (s[len] != '\0')
-		len++;
-
-	return (len);
-}
-
-/**
- * _strdup - Duplicates a string
- *
- * @str: string to be duplicated
- *
- * Return: a pointer to the duplicated string
- */
-
-char *_strdup(char *str)
-{
-	unsigned int i, len;
-	char *dup;
-
-	if (str == NULL)
-		return (NULL);
-
-	len = _strlen(str);
-
-	dup = malloc(sizeof(char) * (len + 1));
-	if (dup == NULL)
-		return (NULL);
-
-	for (i = 0; i < (len + 1); i++)
-		dup[i] = str[i];
-
-	return (dup);
-}
-
-/**
  * free_arr - de-allocates a 2D array in case of failure
  *
  * @arr: array to deallocate
@@ -85,13 +36,13 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	arr = malloc(sizeof(int) * height);
+	arr = malloc(sizeof(int *) * height);
 	if (arr == NULL)
 		return (NULL);
 
 	for (i = 0; i < height; i++)
 	{
-		arr[i] = malloc(sizeof(int *) * width);
+		arr[i] = malloc(sizeof(int) * width);
 		if (arr[i] == NULL)
 		{
 			free_arr(arr, i);
