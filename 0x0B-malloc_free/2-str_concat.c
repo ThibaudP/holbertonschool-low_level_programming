bd9ced1d4d5fa7cd0a8dd3b908b7b72d7d1a78fb
ThibaudP <t.poncin@gmail.com>
2020-10-13 13:23:02 +0000

    Fixed 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -64,7 +64,8 @@ char *str_concat(char *s1, char *s2)
 		return (_strdup(s2));
 	if (s2 == NULL && s1 != NULL)
 		return (_strdup(s1));
-
+	if (s1 == NULL && s2 == NULL)
+		return ("");
 	res = malloc(sizeof(char) * (_strlen(s1) + _strlen(s2) + 1));
 	if (res == NULL)
 		return (NULL);
