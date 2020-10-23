81322fbd1e8c0dfcce208d65a78194a77a9a4fc1
ThibaudP <t.poncin@gmail.com>
2020-10-23 12:38:42 +0000

    Added up to 100

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,55 +1,6 @@
 #include "variadic_functions.h"
 
 /**
- * print_char - prints a char
- *
- * @c: char to print
- */
-
-void print_char(char c)
-{
-	printf("%c", c);
-}
-
-/**
- * print_int - prints an int
- *
- * @i: int to print
- */
-
-void print_int(int i)
-{
-	printf("%i", i);
-}
-
-/**
- * print_float - prints a float
- *
- * @f: float to print
- */
-
-void print_float(float f)
-{
-	printf("%f", f);
-}
-
-
-/**
- * print_string - prints a string
- *
- * @s: string to print
- */
-
-void print_string(char *str)
-{
-	if (str == NULL)
-		str = "(nil)";
-
-	printf("%s", str);
-}
-
-
-/**
  * print_all - mini printf - prints all the strings
  *
  * @format: format constant
@@ -67,21 +18,25 @@ void print_all(const char * const format, ...)
 		{NULL, NULL}
 	};
 	char *sep = "";
+	void *arg;
 
 	va_start(list, format);
-	
-	while (format[i])
+
+	while (format && format[i])
 	{
 		j = 0;
-		while(specs[j].spec)
+		while (specs[j].spec)
 		{
 			if (format[i] == specs[j].spec[0])
 			{
 				printf("%s", sep);
 				sep = ", ";
-				
-				printf(specs[j].pfspec, va_arg(list, void*));
-			
+
+				arg = va_arg(list, void*);
+
+				if (format[i] == 's' && arg == NULL)
+					arg = "(nil)";
+				printf(specs[j].pfspec, arg);
 			}
 		j++;
 		}
