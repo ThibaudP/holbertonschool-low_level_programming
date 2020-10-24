aa75c993ab42a8b5cd5cf8b4c0c7926633914fa3
ThibaudP <t.poncin@gmail.com>
2020-10-24 13:57:08 +0000

    Add NULL case for 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -12,13 +12,16 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list list;
 	unsigned int i;
+	char *buff;
 
 	va_start(list, n);
 	for (i = 0; i < n; i++)
 	{
 		if (i != 0)
 			printf("%s", (separator != NULL) ? separator : "");
-		printf("%s", va_arg(list, char *));
+		buff = va_arg(list, char *);
+
+		printf("%s", (buff != NULL) ? buff : "(nil)");
 	}
 	va_end(list);
 
