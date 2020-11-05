4ef2fa04e9fa1afbd52507fb81f7a1c5a3be2688
ThibaudP <t.poncin@gmail.com>
2020-11-05 17:47:16 +0000

    Add 100

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * get_endianness - checks if numbers are stored as little or big endian
+ *
+ * Return: 1 if big endian, 0 if little endian 
+ */
+
+int get_endianness(void)
+{
+	int i = 1;
+	char *c = (char *)&i;
+
+	if (*c != 0)
+		return (1);
+	else
+		return (0);
+}
