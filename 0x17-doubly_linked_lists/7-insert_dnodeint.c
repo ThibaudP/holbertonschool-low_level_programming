63ce1de11d77f28269ef0767c1a449d41442a2ae
ThibaudP <t.poncin@gmail.com>
2020-12-13 15:30:51 +0000

    Fix 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -45,9 +45,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 			cur->prev = new;
 		}
-		/*else if (cnt == idx)
+		else if (cnt == idx)
 			new = add_dnodeint_end(h, n);
-*/
+
 	}
 	return (new);
 }
