0d1b1123547a96a0fa30526225e926df0185a16f
ThibaudP <t.poncin@gmail.com>
2020-12-13 15:35:43 +0000

    Fix 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,6 +1,28 @@
 #include "lists.h"
 
 /**
+ * list_len - returns the number of elements in a linked list
+ *
+ * @h: pointer to head of a linked list
+ *
+ * Return: the number of nodes in the list
+ */
+
+size_t list_len(dlistint_t *h)
+{
+	unsigned int cnt = 0;
+
+	while (h != NULL)
+	{
+		cnt++;
+		h = h->next;
+	}
+
+	return (cnt);
+}
+
+
+/**
  * delete_dnodeint_at_index - deletes a node at requested index
  *
  * @head: head node to the list
@@ -15,6 +37,9 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int idx)
 	unsigned int cnt = 0;
 
 	if (*head == NULL)
+		return (1);
+
+	if (idx > list_len(*head))
 		return (1);
 
 	while (cur && cnt != idx)
