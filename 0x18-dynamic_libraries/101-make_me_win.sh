1f4a514550a09e9dc556d3e4014b2e1dfb2dbb34
ThibaudP <t.poncin@gmail.com>
2020-12-14 01:18:46 +0000

    Add 2nd part of 101

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+wget -P /tmp -q https://github.com/ThibaudP/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/101_make_me_win.so
+export LD_PRELOAD=/tmp/101_make_me_win.so
