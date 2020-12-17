7a42a79642a5a46c6bbabe81cab82ff7d3455579
ThibaudP <t.poncin@gmail.com>
2020-12-17 20:45:24 +0000

    Add placeholder for 103

---
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - keygen for crackme5... or not
+ *
+ * @argc: number of args to main
+ * @argv: args to main
+ *
+ * Return: always EXIT_SUCCESS
+ */
+
+int main(int argc, char **argv)
+{
+    if(argc == 2)
+        puts(argv[1]);
+
+    return (EXIT_SUCCESS);
+}
