8cd7d3c81266e55ba277dcf51bd71b839afc950b
ThibaudP <t.poncin@gmail.com>
2021-04-20 15:09:37 +0000

    fix?

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -33,7 +33,7 @@ int jump_search(int *array, size_t size, int value)
 	if (end >= size)
 		end = size - 1;
 
-	while  (strt <= end)
+	while  (strt <= end && value < array[end])
 	{
 		printf("Value checked array[%lu] = [%d]\n", strt, array[strt]);
 		if (array[strt] == value)
