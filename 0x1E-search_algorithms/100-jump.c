5be174de032c0af329d41bcca1d11a319a3c6628
ThibaudP <t.poncin@gmail.com>
2021-04-20 17:16:42 +0000

    betty

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -29,7 +29,7 @@ int jump_search(int *array, size_t size, int value)
 	printf("Value found between indexes [%lu] and [%lu]\n", jdx, idx);
 
 	idx = idx >= size ? size - 1 : idx;
-	
+
 	while (jdx <= idx)
 	{
 		printf("Value checked array[%lu] = [%d]\n", jdx, array[jdx]);
